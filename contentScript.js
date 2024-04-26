07544fa466510aa8a562febbb9d7cce9fa8b1681
Alen Rozac <alenrozac@gmail.com>
2024-04-26 13:51:02 +0200

    attempt at map

--- contentScript.js
+++ contentScript.js
@@ -2,41 +2,39 @@
 window.addEventListener("load", function () {
   // Wait for the page to fully load
   const mapBox = document.querySelector("#lu_map");
+  const intMap = document.querySelector("#dimg_1");
 
-  if (mapBox) {
-    // console.log("Mapbox present");
-
+  // if mapbox or intmap
+  if (mapBox || intMap) {
     // Extract query parameter value from current webpage URL
     function getQueryParamValue(url, paramName) {
-      // Extract query parameters from URL
       const queryString = url.split("?")[1];
-      // Split query string into key-value pairs
       const queryParams = new URLSearchParams(queryString);
-      // Return the value associated with the given parameter name
       return queryParams.get(paramName);
     }
+
     const currentUrl = window.location.href;
     const searchTerm = getQueryParamValue(currentUrl, "q");
-    // console.log("Search term:", searchTerm);
 
     // Generate Google Maps search link with a query
     function generateGoogleMapsLink(query) {
-      // Encode the query parameter
       const encodedQuery = encodeURIComponent(query);
-      // Construct the Google Maps search URL with the encoded query
       const googleMapsLink = `https://www.google.com/maps/search/${encodedQuery}`;
       return googleMapsLink;
     }
 
     // Generate Google Maps search link with the query
     const mapsLink = generateGoogleMapsLink(searchTerm);
-    // console.log("Google Maps search link:", mapsLink);
 
-    mapBox.addEventListener("click", function () {
-      // Open a new tab with the Google Maps search link
-      window.open(mapsLink, "_blank");
-    });
-  } else {
-    console.log("Mapbox not present");
+    if (mapBox) {
+      mapBox.addEventListener("click", function () {
+        window.open(mapsLink, "_blank");
+      });
+    }
+    if (intMap) {
+      intMap.addEventListener("click", function () {
+        window.open(mapsLink, "_blank");
+      });
+    }
   }
 });
